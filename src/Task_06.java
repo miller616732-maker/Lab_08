556b1fa561e0d8b5c4888fe9d9d8498fd44a510e
616732 <miller.616732@swl.k12.oh.us>
2025-11-06 08:32:01 -0500

    11/6/25 8:31

--- src/Task_06.java
+++ src/Task_06.java
@@ -1,3 +1,15 @@
-public class Task_06 {
+import java.util.Scanner;
 
+public class Task_06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double yourC = InputHelper.getDouble(scan, "Enter the degrees in celsius");
+        double yourF = celsiusToFahrenheit(yourC);
+        System.out.printf("%-10s%5.2f", "Your temperature in fahrenheit is: ", yourF);
+    }
+    public static double celsiusToFahrenheit(double Celsius) {
+        double fahrenheitDegrees;
+        fahrenheitDegrees = ((Celsius * 9/5) + 32);
+        return fahrenheitDegrees;
+    }
 }
